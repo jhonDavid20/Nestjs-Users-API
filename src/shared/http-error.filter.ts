bf184e4816cb026d48194b3576f995bfa0ac71e0
Jhonathan David <jhonps396@gmail.com>
2018-11-03 16:21:44 -0400

    Adding Error handler and loggin interceptors

---
+++ src/shared/http-error.filter.ts
@@ -0,0 +1,26 @@
+import { Catch, ExceptionFilter, HttpException, ArgumentsHost, Logger } from '@nestjs/common';
+
+@Catch()
+export class HttpErrorFilter implements ExceptionFilter{
+    catch(exception: HttpException, host: ArgumentsHost){
+        const ctx = host.switchToHttp();
+        const request = ctx.getRequest();
+        const response = ctx.getResponse();
+        const status = exception.getStatus();
+
+        const errorResponse = {
+            code: status,
+            timestamp: new Date().toLocaleDateString(),
+            path: request.method,
+            message: exception.message.error || exception.message || null
+        }
+
+        Logger.error(
+            `${request.method} ${request.url}`,
+           JSON.stringify(errorResponse),
+            'ExceptioFilter'
+        );
+
+        response.status(404).json(errorResponse);
+    }
+}
